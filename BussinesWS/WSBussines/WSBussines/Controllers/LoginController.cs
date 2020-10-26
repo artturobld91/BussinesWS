e54b2305ca4513d0cee8826ae67c7be5ab8583b8
artturobld@gmail.com <G0tmilk?github>
2020-10-25 21:35:02 -0700

    Changes to Login Controller

--- BussinesWS/WSBussines/WSBussines/Controllers/LoginController.cs
+++ BussinesWS/WSBussines/WSBussines/Controllers/LoginController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data;
+using System.Data.Odbc;
 
 namespace WSBussines.Controllers
 {
@@ -27,6 +29,34 @@ namespace WSBussines.Controllers
         public string LoginClient(string username, string password)
         {
             string value = string.Format("Username: {0} - Password: {1}", username, password);
+
+            try
+            {
+                using (OdbcConnection connection = new OdbcConnection("DSN=BussinesDB;UID=postgres;PWD=password"))
+                {
+                    connection.Open();
+                    using (OdbcCommand command = connection.CreateCommand())
+                    { 
+                        command.CommandText = string.Format("Select * from public.Users where email = '{0}' and user_password = '{1}'", username, password);
+                        using (OdbcDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                value += @"\n Login Successful";
+                            }
+                            else
+                            {
+                                value += @"\n Loggin Failed";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+            }
+
             return value;
         }
 
